1d7cf3d200ada3fd7ef97ab802386597857ffe21
ozra <petar.ozretic@mail.com>
2017-11-15 20:34:38 +0100

    Save no.2, no bugs so far XD

--- aplication.js
+++ aplication.js
@@ -1,13 +1,21 @@
+let indikator = false;
+let brojac = 0;
+let dozvola = true;
 $(document).ready(function() {
 
   $('#wm').on('click', function() {
     let pom = +$('#wt').text();
     if (pom > 1) {
       pom = pom - 1;
-    }
-    if(pom <10)
-      pom = "0"+pom
+    };
     $('#wt').text(pom);
+    if(pom <10)
+      {$('#wt').prepend('&nbsp;');};
+    if(dozvola === true){
+        $('#vrime').text($('#wt').text()+":00");
+        $("#vrime").prepend("&nbsp;");
+      }
+
   });
 
   $('#wp').on('click', function() {
@@ -16,6 +24,13 @@ $(document).ready(function() {
       pom = pom + 1;
     else alert("Really? If you push it so far it kinda looses the sence...")
     $('#wt').text(pom);
+    if(pom <10)
+      $('#wt').prepend('&nbsp;');
+      if(dozvola === true){
+          $('#vrime').text($('#wt').text()+":00");
+          $("#vrime").prepend("&nbsp;");
+        }
+
   });
 
   $('#bm').on('click', function() {
@@ -34,4 +49,74 @@ $(document).ready(function() {
     $('#bt').text(pom);
   });
 
+  $('#loop').on('click', function() {
+    $('#bt').text(5);
+    $('#wt').text(25);
+    $('#radnja').text("WORK");
+    $('#vrime').text("25:00");
+    $("#vrime").prepend("&nbsp;");
+    indikator = false;
+    dozvola = true;
+  });
+
+  $('#stop').on('click', function() {
+    $('#radnja').text("WORK");
+    let pom = +$('#wt').text();
+    $('#vrime').text(pom+":00");
+    if(pom > 9)
+      $("#vrime").prepend("&nbsp;");
+    else {
+      $("#vrime").prepend("&nbsp;&nbsp;");
+    }
+    indikator = false;
+    dozvola = true;
+  });
+
+  $('#play').on('click', function() {
+    indikator = true;
+    dozvola = false;
+  });
+  $('#pause').on('click', function() {
+    indikator = false;
+  });
+
+setInterval(function(){
+  if(indikator === true)
+    {
+      let pom = $('#vrime').text().split(':');
+      let min = +pom[0];
+      let sek = +pom[1];
+      if( min !== 0 || sek !== 0)
+      {
+        if(sek === 0 && min !== 0)
+        {
+          min = min - 1;
+          sek = 60;
+        }
+        sek = sek - 1;
+      }
+      else
+      {
+          if($('#radnja').text() === "WORK")
+          {
+            $('#radnja').text("BREAK");
+            min = +$('#bt').text();
+          }
+          else
+          {
+            $('#radnja').text("WORK");
+            min = +$('#wt').text();
+          }
+      }
+
+      if(sek<10)
+      {sek = "0"+sek;}
+      $('#vrime').text(min+":"+sek);
+      $("#vrime").prepend("&nbsp;");
+      if(min < 10)
+      {$("#vrime").prepend("&nbsp;");}
+
+    }
+
+}, 1000);
 });
